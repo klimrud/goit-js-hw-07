2e623358dd3abfc92cf190ca9c95e79fabc04ef6
Tetiana Klimenko <ktlimenko@gmail.com>
2023-01-21 03:32:15 +0200

    dz-07-markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+// console.log(...galleryItems)
+
+const container = document.querySelector('.gallery');
+
+function imageGelleryMarkup(galleryItem) {
+    return  galleryItem.map( ({preview, original, description}) =>{
+    return `<div class="gallery__item">
+              <a class="gallery__link" href="${original}">
+                <img
+                 class="gallery__image"
+                 src="${preview}"
+                 data-source="${original}"
+                 alt="${description}"
+                />
+             </a>
+           </div>`;
+  }).join('');
+  
+};
+// console.log();
+// console.log()
+container.insertAdjacentHTML('beforeend', imageGelleryMarkup(galleryItems) )
+//  console.log()
+
+console.log(container);
+
+container.addEventListener( 'click', getGallery );
+
+function getGallery(){
+
+};
--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+function imageGelleryMarkup(galleryItem) {
+    return  galleryItem.map( ({preview, original, description}) =>{
+    return `<li>
+              <a class="gallery__item" href="${original}">
+                <img
+                 class="gallery__image"
+                 src="${preview}"
+                 data-source="${original}"
+                 alt="${description}"
+                />
+             </a>
+             </li>`;
+  }).join('');
+  
+};
+// console.log();
+ 
+galleryEl.insertAdjacentHTML('beforeend', imageGelleryMarkup(galleryItems) )
+console.log(galleryEl)
